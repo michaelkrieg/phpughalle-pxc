1fde884cea7eef38870c323c8437717525db6f86
Michael Krieg <mk@red-tag.de>
2014-02-09 16:48:47 +0100

    Percona APT Repository einbinden für alle Cluste Nodes

--- local_modules/percona-cluster/manifests/init.pp
+++ local_modules/percona-cluster/manifests/init.pp
@@ -10,6 +10,14 @@ class pxc_base {
   class { 'apt':    }
   class { 'ntp':    }
 
+  apt::source { "percona":
+      key               => "1C4CBDCDCD2EFD2A",
+      location          => "http://repo.percona.com/apt",
+      release           => "wheezy",
+      repos             => "main",
+      required_packages => "debian-keyring debian-archive-keyring",
+      include_src       => true
+    }
 }
 
 class percona-cluster inherits pxc_base {
